3c7ec8c49941e0824af76351d200104cfb2dd0a7
Cyrus <c@mailcyr.us>
2015-10-03 13:55:08 -0400

    deploy, row init, farm tile for 1 block and edit block all seem to work.

--- contracts/etheria.sol
+++ contracts/etheria.sol
@@ -5,107 +5,99 @@ contract Etheria is mortal {
     address creator;
     uint8 mapsize = 17;
     Tile[17][17] tiles;
-    bool ownersinitialized = false;
-    bool pricesinitialized = false;
-    bool elevationsinitialized = false;
-    bool[17] ownerrowsinitialized;
-    bool[17] elevationrowsinitialized;
-    bool[17] pricerowsinitialized;
+    bool allrowsinitialized;
+    bool[17] rowsinitialized;
     
     // TODO: 
-    // 3. Mouseover
-    // 4. Block locations & colors
+    // Mine tile
+    // Fitness vote
+    // Cast threat
     
     struct Tile 
     {
     	uint8 elevation;
     	address owner;
     	uint80 price; // 0 = not for sale. 0-4700000000000000000000 wei (approx) (0-4700 ether)
-    	int8[] blocks; // index 0 = which, index 1 = blockx, index 2 = blocky, index 3 = blockz
+    	int8[] blocks; // index 0 = which, index 1 = blockx, index 2 = blocky, index 3 = blockz (< 0 = not yet placed)
     	               // index 4 = r, index 5 = g, index 6 = b
     }
     
     function Etheria() 
     {
         creator = msg.sender;
-        for(uint8 x = 0; x < mapsize; x++)
-        {
-        	ownerrowsinitialized[x] = false;
-        	elevationrowsinitialized[x] = false;
-        }	
     }
     
-    function initializeOwners(uint8 row)
+    function initializeRow(uint8 row, uint8[17] _elevations)
     {
-    	if(ownersinitialized == true)
+    	if(allrowsinitialized == true)
     		return;
     	
     	if(row >= mapsize) // this row index is out of bounds.
     		return; 
     	
     	for(uint8 x = 0; x < mapsize; x++)
-    		tiles[x][row].owner = creator; 
-    	ownerrowsinitialized[row] = true;
+    	{
+    	    tiles[x][row].owner = creator;
+    	    tiles[x][row].price = 250000000000000000; //.25 eth
+    	    tiles[x][row].elevation = _elevations[x];
+    	}
+    	rowsinitialized[row] = true;
     	
     	for(uint r = 0; r < mapsize; r++)
     	{
-    		if(ownerrowsinitialized[r] == false) // at least one row is not yet initialized. Return.
+    		if(rowsinitialized[r] == false) // at least one row is not yet initialized. Return.
     			return;
     	}	
-        ownersinitialized = true;
+    	allrowsinitialized = true;
     }
     
-    function initializePrices(uint8 row)
+    function farmTile(uint8 x, uint8 y)
     {
-    	if(pricesinitialized == true)
-    		return;
-    	
-    	if(row >= mapsize) // this row index is out of bounds.
-    		return; 
-    	
-    	for(uint8 x = 0; x < mapsize; x++)
-    		tiles[x][row].price = 250000000000000000; //.25 eth
-    	pricerowsinitialized[row] = true;
-    	
-    	for(uint r = 0; r < mapsize; r++)
-    	{
-    		if(pricerowsinitialized[r] == false) // at least one row is not yet initialized. Return.
-    			return;
-    	}	
-    	pricesinitialized = true;
-    }
-    
-    function initializeElevations(uint8 row, uint8[17] _elevations)
-    {
-    	if(elevationsinitialized == true)
-    		return;
-    	
-    	if(row >= mapsize) // this row index is out of bounds.
-    		return; 
-    	
-    	for(uint8 x = 0; x < mapsize; x++)
-    		tiles[x][row].elevation = _elevations[x]; 
-    	elevationrowsinitialized[row] = true;
-    	
-    	for(uint r = 0; r < mapsize; r++)
-    	{
-    		if(elevationrowsinitialized[r] == false) // at least one row is not yet initialized. Return.
-    			return;
-    	}	
-    	elevationsinitialized = true;
-    }
-    
-    function addBlock(uint8 x, uint8 y, int8[7] block)
-    {
+        if(tiles[x][y].owner != msg.sender)
+            return;
+        uint lastblocknumberused = block.number - 1 ;
+    	uint128 lastblockhashused_uint128 = uint128(block.blockhash(lastblocknumberused));
+    	uint128 remainder256 = lastblockhashused_uint128 % 256;
+    	// index 0 = which, index 1 = blockx, index 2 = blocky, index 3 = blockz, index 4 = r, index 5 = g, index 6 = b
     	Tile current = tiles[x][y];
     	current.blocks.length+=7;
-    	current.blocks[current.blocks.length - 7] = block[0];
-    	current.blocks[current.blocks.length - 6] = block[1];
-    	current.blocks[current.blocks.length - 5] = block[2];
-    	current.blocks[current.blocks.length - 4] = block[3];
-    	current.blocks[current.blocks.length - 3] = block[4];
-    	current.blocks[current.blocks.length - 2] = block[5];
-    	current.blocks[current.blocks.length - 1] = block[6];
+    	if(remainder256 == 0)
+    	{
+    	    // ACTION BLOCK!
+    	    current.blocks[current.blocks.length - 7] = 14;
+    	    current.blocks[current.blocks.length - 6] = 3;
+    	    current.blocks[current.blocks.length - 5] = 3;
+    	    current.blocks[current.blocks.length - 4] = -1;
+    	    current.blocks[current.blocks.length - 3] = -128;
+    	    current.blocks[current.blocks.length - 2] = 0;
+    	    current.blocks[current.blocks.length - 1] = -128;
+    	}
+    	else // normal block
+    	{
+    	    current.blocks[current.blocks.length - 7] = 0;
+    	    current.blocks[current.blocks.length - 6] = -3;
+    	    current.blocks[current.blocks.length - 5] = -3;
+    	    current.blocks[current.blocks.length - 4] = -1;
+    	    current.blocks[current.blocks.length - 3] = 0;
+    	    current.blocks[current.blocks.length - 2] = -128;
+    	    current.blocks[current.blocks.length - 1] = -128;
+    	}
+    }
+    
+    // NOTE: In this instance, block[0] is irrelevant. We can't change which type of block it is
+    function editBlock(uint8 x, uint8 y, uint indexOfBlockToEdit, int8[7] block)  
+    {
+        if(tiles[x][y].owner != msg.sender)
+            return;
+    	Tile current = tiles[x][y];
+    	uint metaindex = indexOfBlockToEdit*7;
+        //current.blocks[metaindex] = error; // irrelevant. We can't change which type of block it is
+    	current.blocks[metaindex + 1] = block[1];
+    	current.blocks[metaindex + 2] = block[2];
+    	current.blocks[metaindex + 3] = block[3];
+    	current.blocks[metaindex + 4] = block[4];
+    	current.blocks[metaindex + 5] = block[5];
+    	current.blocks[metaindex + 6] = block[6];
     	return;
     }
     
@@ -113,6 +105,12 @@ contract Etheria is mortal {
     {
     	if(tiles[x][y].owner == msg.sender)
     		tiles[x][y].owner == newowner;
+    }
+    
+    function getBlocksForTile(uint8 x, uint8 y) constant returns (int8[])
+    {
+    	Tile memory currenttile = tiles[x][y];
+    	return currenttile.blocks;
     }
     
     function getElevations() constant returns (uint8[17][17])
@@ -135,16 +133,10 @@ contract Etheria is mortal {
         {
         	for(uint8 x = 0; x < mapsize; x++)
         	{
-        		owners[x][y] = tiles[x][y].owner; 
+        	    owners[x][y] = tiles[x][y].owner; 
         	}	
         }	
     	return owners;
-    }
-    
-    function getBlocksForTile(uint8 x, uint8 y) constant returns (int8[])
-    {
-    	Tile memory currenttile = tiles[x][y];
-    	return currenttile.blocks;
     }
     
     function getPrices() constant returns(uint80[17][17])
