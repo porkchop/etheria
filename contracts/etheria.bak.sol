d591d1684bee648306d20c66af88b341382dc3f0
cyrus <c@mailcyr.us>
2015-10-14 23:06:23 -0400

    Finished but too large to test.

--- contracts/etheria.bak.sol
+++ contracts/etheria.bak.sol
@@ -26,11 +26,9 @@ contract Etheria //is EtheriaHelper
     	uint lastfarm;
     }
     
-   // EtheriaHelper eh;
 	
     function Etheria() {
     	creator = msg.sender;
-    	//eh = EtheriaHelper(0xf20c9fa34847f6bc42b2f60014268bec65676af7);
     }
     
     function setInitializer(address _i)
@@ -56,19 +54,19 @@ contract Etheria //is EtheriaHelper
     {
     	if(msg.sender != initializer)
     		return;
-    	for(uint8 col = 0; col < mapsize; col++)
-    		tiles[col][row].elevation = _elevations[col];
+    	for(uint8 x = 0; x < mapsize; x++)
+    		tiles[x][row].elevation = _elevations[x];
     }
     
     
     function getElevations() constant returns (uint8[17][17])
     {
         uint8[17][17] memory elevations;
-        for(uint8 row = 0; row < mapsize; row++)
+        for(uint8 y = 0; y < mapsize; y++)
         {
-        	for(uint8 col = 0; col < mapsize; col++)
+        	for(uint8 x = 0; x < mapsize; x++)
         	{
-        		elevations[col][row] = tiles[col][row].elevation; 
+        		elevations[x][y] = tiles[x][y].elevation; 
         	}	
         }	
     	return elevations;
@@ -77,11 +75,11 @@ contract Etheria //is EtheriaHelper
     function getOwners() constant returns(address[17][17])
     {
         address[17][17] memory owners;
-        for(uint8 row = 0; row < mapsize; row++)
+        for(uint8 y = 0; y < mapsize; y++)
         {
-        	for(uint8 col = 0; col < mapsize; col++)
+        	for(uint8 x = 0; x < mapsize; x++)
         	{
-        	    owners[col][row] = tiles[col][row].owner; 
+        	    owners[x][y] = tiles[x][y].owner; 
         	}	
         }	
     	return owners;
@@ -106,92 +104,73 @@ contract Etheria //is EtheriaHelper
     		return uint8(((uint(_b32) % (16 ** (64 - (byteindex*2)))) - ((uint(_b32) % (16 ** (64 - (byteindex*2)))) % (16 ** (64 - 2 - (byteindex*2))))) / (16 ** (64 - 2 - (byteindex*2)))); 	
     }
     
-    function farmTile(uint8 col, uint8 row)
+    function farmTile(uint8 x, uint8 y)
     {
-        if(tiles[col][row].owner != msg.sender)
+        if(tiles[x][y].owner != msg.sender)
             return;
-        if((block.number - tiles[col][row].lastfarm) < 4320) // a day's worth of blocks hasn't passed yet. can only farm once a day. (Assumes block times of 20 seconds.)
+        if((block.number - tiles[x][y].lastfarm) < 4320) // a day's worth of blocks hasn't passed yet. can only farm once a day. (Assumes block times of 20 seconds.)
         	return;
         bytes32 lastblockhash = block.blockhash(block.number - 1);
-    	for(uint8 i = 0; i < 20; i++)
+    	for(uint8 i = 0; i < 10; i++)
     	{
-            tiles[col][row].blocks.length+=5;
-    	    tiles[col][row].blocks[tiles[col][row].blocks.length - 1][0] = int8(getUint8FromByte32(lastblockhash,i) % 32); // which, guaranteed 0-31
-    	    tiles[col][row].blocks[tiles[col][row].blocks.length - 1][1] = 0; // x
-    	    tiles[col][row].blocks[tiles[col][row].blocks.length - 1][2] = 0; // y
-    	    tiles[col][row].blocks[tiles[col][row].blocks.length - 1][3] = -1; // z
-    	    tiles[col][row].blocks[tiles[col][row].blocks.length - 1][4] = 0; // color
+            tiles[x][y].blocks.length+=5;
+    	    tiles[x][y].blocks[tiles[x][y].blocks.length - 1][0] = int8(getUint8FromByte32(lastblockhash,i) % 32); // which, guaranteed 0-31
+    	    tiles[x][y].blocks[tiles[x][y].blocks.length - 1][1] = 0; // x
+    	    tiles[x][y].blocks[tiles[x][y].blocks.length - 1][2] = 0; // y
+    	    tiles[x][y].blocks[tiles[x][y].blocks.length - 1][3] = -1; // z
+    	    tiles[x][y].blocks[tiles[x][y].blocks.length - 1][4] = 0; // color
     	}
-    	tiles[col][row].lastfarm = block.number;
+    	tiles[x][y].lastfarm = block.number;
     }
     
-    function editBlock(uint8 col, uint8 row, uint index, int8[5] block)  
+    // NOTE: In this instance, block[0] is irrelevant. We can't change "which" type of block it is
+    function editBlock(uint8 coordx, uint8 coordy, uint index, int8[5] block)  
     {
-        if(tiles[col][row].owner != msg.sender)
+        if(tiles[coordx][coordy].owner != msg.sender)
             return;
-        if(block[3] < -1) // Can't hide blocks or change configuration of hidden blocks. This limitation is to prevent massive reorganization of occupado. 
-        	return;
-        block[0] = tiles[col][row].blocks[index][0]; // can't change the which, so set it to whatever it already was
-              
-    	if(!wouldFallOutside(block[0],block[1],block[2]) && touchesAndAvoidsOverlap(col,row,block[0],block[1],block[2],block[3]))
-        {	// the new placement is valid
-        	// get the proposed new 8 hex locations
-         	int8[3][8] wouldoccupy = blocks[uint(block[0])].occupies;
-         	for(uint8 b = 0; b < 8; b++) // always 8 hexes
-         	{
-         		wouldoccupy[b][0] = wouldoccupy[b][0]+block[1];
-         		wouldoccupy[b][1] = wouldoccupy[b][1]+block[2];
-         		if(wouldoccupy[0][1] % 2 != 0 && wouldoccupy[b][1]%2 != 0)
-         			wouldoccupy[b][0] = wouldoccupy[b][0]+1; // anchor y and this hex y are both odd, offset by +1
-         		wouldoccupy[b][2] = wouldoccupy[b][2]+block[3];
-         	}
-        	
-         	if(tiles[col][row].blocks[index][3] >= 0) // If the previous z was greater than 0 (i.e. not hidden) ...
-         	{
-         		// get the previous 8 hex locations
-         		int8[3][8] didoccupy = blocks[uint(block[0])].occupies;
-             	for(uint8 a = 0; a < 8; a++) // always 8 hexes
-             	{
-             		didoccupy[a][0] = didoccupy[a][0]+tiles[col][row].blocks[index][1];
-             		didoccupy[a][1] = didoccupy[a][1]+tiles[col][row].blocks[index][2];
-             		if(didoccupy[0][1] % 2 != 0 && didoccupy[a][1]%2 != 0)
-             			didoccupy[a][0] = didoccupy[a][0]+1; // anchor y and this hex y are both odd, offset by +1
-             		didoccupy[a][2] = didoccupy[a][2]+tiles[col][row].blocks[index][3];
-             	}
-             	
-             	for(uint8 l = 0; l < 8; l++) // loop 8 times,find the previous occupado entries and overwrite them
-             	{
-             		for(uint o = 0; o < occupado[col][row].length; o++)
-             		{
-             			if(didoccupy[l][0] == occupado[col][row][o][0] && didoccupy[l][1] == occupado[col][row][o][1] && didoccupy[l][2] == occupado[col][row][o][2]) // are the arrays equal?
-             			{
-             				occupado[col][row][o] = wouldoccupy[l]; // found it. Overwrite it
-             			}
-             		}
-             	}
-         	}
-         	else // previous block was hidden
-         	{
-         		for(uint8 ll = 0; ll < 8; ll++) // add the 8 new hexes to occupado
-             	{
-         			occupado[col][row].length++;
-         			occupado[col][row][occupado[col][row].length-1] = wouldoccupy[ll];
-             	}
-         	}	
-         	tiles[col][row].blocks[index] = block;
+        if(block[3] < 0) // CURRENT LIMITATION - to avoid deletion from occupado and costly array re-adjustment, once a block is visible, it can never be re-hidden, only moved
+            return;
+        
+        block[0] = tiles[coordx][coordy].blocks[index][0]; // can't change the which, so set block[0] to what it already is
+        
+        // DETERMINE VALIDITY OF NEW PLACEMENT
+    	int8[3][8] memory wouldoccupy = getOccupies(block[0], block[1], block[2], block[3]);
+    	int8[3][] memory surroundings = getSurroundings(block[0], block[1], block[2], block[3]);
+        if(!wouldFallOutside(wouldoccupy) && touchesAndAvoidsOverlap(coordx,coordy,wouldoccupy,surroundings))
+        {	
+        	if(tiles[coordx][coordy].blocks[index][3] >= 0) // the new placement is valid. If the previous z was greater than 0 (i.e. not hidden) ...
+        	{
+        		int8[3][8] memory previous8occupied = getOccupies(tiles[coordx][coordy].blocks[index][0], tiles[coordx][coordy].blocks[index][1] ,tiles[coordx][coordy].blocks[index][2] ,tiles[coordx][coordy].blocks[index][3]);
+        		for(uint8 l = 0; l < 8; l++) // loop 8 times,find the previous occupado entries and overwrite them
+            	{
+            		for(uint o = 0; o < occupado[coordx][coordy].length; o++)
+            		{
+            			if(previous8occupied[l][0] == occupado[coordx][coordy][o][0] && previous8occupied[l][1] == occupado[coordx][coordy][o][1] && previous8occupied[l][2] == occupado[coordx][coordy][o][2]) // are the arrays equal?
+            			{
+            				occupado[coordx][coordy][o] = wouldoccupy[l]; // found it. Overwrite it
+            			}
+            		}
+            	}
+        	}
+        	else // previous block was hidden
+        	{
+        		for(uint8 ll = 0; ll < 8; ll++) // add the 8 new hexes to occupado
+            	{
+        			occupado[coordx][coordy].length++;
+        			occupado[coordx][coordy][occupado[coordx][coordy].length-1] = wouldoccupy[ll];
+            	}
+        	}	
+        	tiles[coordx][coordy].blocks[index] = block;
         }
-        // else not a valid block location, return
     	return;
     }
     
-    function getBlocks(uint8 col, uint8 row) constant returns (int8[5][])
+    function getBlocks(uint8 coordx, uint8 coordy) constant returns (int8[5][])
     {
-    	return tiles[col][row].blocks;
+    	return tiles[coordx][coordy].blocks;
     }
     
     // block edit validation
-    
-    
     
     // TODO:
     // DONE block texturing
@@ -337,27 +316,7 @@ contract Etheria //is EtheriaHelper
 		}	
     }
     
-    function wouldFallOutside(int8 which, int8 x, int8 y) private constant returns (bool)
-    {
-    	int8 occupiesx = 0;
-    	int8 occupiesy = 0;
-    	
-    	for(uint8 b = 0; b < 8; b++) // always 8 hexes
-    	{
-    		occupiesx = blocks[uint(which)].occupies[b][0];
-    		occupiesy = blocks[uint(which)].occupies[b][1];
-    		if(y % 2 != 0 && occupiesy%2 != 0) // if y is odd, offset the x by 1
-    		{
-    			occupiesx = occupiesx + 1;
-    		}
-    		address hexCoordValidator = 0x18b84dfffa22fc3bf502cc46ac64d13306df4d41;
-    		if(!blockHexCoordsValid(occupiesx+x, occupiesy+y))
-    			return true;
-    	}
-    	return false;
-    }
-    
-    function touchesAndAvoidsOverlap(uint8 col, uint8 row, int8 which, int8 x, int8 y, int8 z) private constant returns (bool)
+    function getOccupies(int8 which, int8 x, int8 y, int8 z) private constant returns (int8[3][8])
     {
     	int8[3][8] wouldoccupy = blocks[uint(which)].occupies;
     	for(uint8 b = 0; b < 8; b++) // always 8 hexes
@@ -368,9 +327,13 @@ contract Etheria //is EtheriaHelper
     			wouldoccupy[b][0] = wouldoccupy[b][0]+1; // anchor y and this hex y are both odd, offset by +1
     		wouldoccupy[b][2] = wouldoccupy[b][2]+z;
     	}
-    	
+    	return wouldoccupy;
+    }
+    
+    function getSurroundings(int8 which, int8 x, int8 y, int8 z) private constant returns (int8[3][])
+    {
     	int8[3][] surroundings = blocks[uint(which)].surroundedby;
-    	for(b = 0; b < surroundings.length; b++)
+    	for(uint8 b = 0; b < surroundings.length; b++)
     	{
     		surroundings[b][0] = surroundings[b][0]+x;
     		surroundings[b][1] = surroundings[b][1]+y;
@@ -378,6 +341,23 @@ contract Etheria //is EtheriaHelper
     			surroundings[b][0] = surroundings[b][0]+1; // anchor y and this hex y are both odd, offset by +1
     		surroundings[b][2] = surroundings[b][2]+z;
     	}
+    	return surroundings;
+    }
+    
+    function wouldFallOutside(int8[3][8] wouldoccupy) private constant returns (bool)
+    {
+    	for(uint8 b = 0; b < 8; b++) // always 8 hexes
+    	{
+    		if(!blockHexCoordsValid(wouldoccupy[b][0], wouldoccupy[b][1]))
+    			return true;
+    	}
+    	return false;
+    }
+    
+    function touchesAndAvoidsOverlap(uint8 coordx, uint8 coordy, int8[3][8] wouldoccupy, int8[3][] surroundings) private constant returns (bool)
+    {
+//    	int8[3][8] wouldoccupy = getOccupies(which, x, y, z);
+//    	int8[3][] surroundings = getSurroundedBy(which, x, y, z);
     	
     	bool touches;
     	uint numloops = 8;
@@ -386,11 +366,11 @@ contract Etheria //is EtheriaHelper
     	
     	for(uint8 l = 0; l < numloops; l++)
     	{
-    		for(uint o = 0; o < occupado[col][row].length; o++)
+    		for(uint o = 0; o < occupado[coordx][coordy].length; o++)
     		{
-    			if(l < 8 && wouldoccupy[l][0] == occupado[col][row][o][0] && wouldoccupy[l][1] == occupado[col][row][o][1] && wouldoccupy[l][2] == occupado[col][row][o][2]) // are the arrays equal?
+    			if(l < 8 && wouldoccupy[l][0] == occupado[coordx][coordy][o][0] && wouldoccupy[l][1] == occupado[coordx][coordy][o][1] && wouldoccupy[l][2] == occupado[coordx][coordy][o][2]) // are the arrays equal?
 					return false; // this hex conflicts. The proposed block does not avoid overlap. Return false immediately.
-    			if(touches == false && l < surroundings.length && surroundings[l][0] == occupado[col][row][o][0] && surroundings[l][1] == occupado[col][row][o][1] && surroundings[l][2] == occupado[col][row][o][2]) // are the arrays equal?
+    			if(touches == false && l < surroundings.length && surroundings[l][0] == occupado[coordx][coordy][o][0] && surroundings[l][1] == occupado[coordx][coordy][o][1] && surroundings[l][2] == occupado[coordx][coordy][o][2]) // are the arrays equal?
     				touches = true;
     		}
     		if(l >= 8 && touches == true)
@@ -470,7 +450,7 @@ contract Etheria //is EtheriaHelper
      *                                
      */
     
-    function makeOffer(uint8 col, uint8 row)
+    function makeOffer(uint8 x, uint8 y)
     {
     	if(msg.value < 10000000000000000 || msg.value > 1208925819614629174706175) // .01 ether up to (2^80 - 1) wei is the valid range
     	{
@@ -478,7 +458,7 @@ contract Etheria //is EtheriaHelper
     			msg.sender.send(msg.value); 		// return their money
     		return;
     	}
-    	else if(tiles[col][row].elevation >= 125 && tiles[col][row].owner == address(0)) // if unowned and above sea level, accept offer of 1 ETH immediately
+    	else if(tiles[x][y].elevation >= 125 && tiles[x][y].owner == address(0)) // if unowned and above sea level, accept offer of 1 ETH immediately
     	{
     		if(msg.value != 1000000000000000000) // 1 ETH is the starting value. If not enough or too much...
     		{
@@ -488,84 +468,83 @@ contract Etheria //is EtheriaHelper
     		else
     		{
     			creator.send(msg.value);     		 // this was a valid offer, send money to contract owner
-    			tiles[col][row].owner = msg.sender;  // set tile owner to the buyer
+    			tiles[x][y].owner = msg.sender;  // set tile owner to the buyer
     			return;		
     		}	
     	}	
     	else
     	{
-    		if(tiles[col][row].offerers.length < 10) // this tile can only hold 10 offers at a time
+    		if(tiles[x][y].offerers.length < 10) // this tile can only hold 10 offers at a time
     		{
-    			for(uint8 i = 0; i < tiles[col][row].offerers.length; i++)
+    			for(uint8 i = 0; i < tiles[x][y].offerers.length; i++)
     			{
-    				if(tiles[col][row].offerers[i] == msg.sender) // user has already made an offer. Update it and return;
+    				if(tiles[x][y].offerers[i] == msg.sender) // user has already made an offer. Update it and return;
     				{
-    					msg.sender.send(tiles[col][row].offers[i]); // return their previous money
-    					tiles[col][row].offers[i] = msg.value; // set the new offer
+    					msg.sender.send(tiles[x][y].offers[i]); // return their previous money
+    					tiles[x][y].offers[i] = msg.value; // set the new offer
     					return;
     				}
     			}	
     			// the user has not yet made an offer
-    			tiles[col][row].offerers.length++; // make room for 1 more
-    			tiles[col][row].offers.length++; // make room for 1 more
-    			tiles[col][row].offerers[tiles[col][row].offerers.length - 1] = msg.sender; // record who is making the offer
-    			tiles[col][row].offers[tiles[col][row].offers.length - 1] = msg.value; // record the offer
+    			tiles[x][y].offerers.length++; // make room for 1 more
+    			tiles[x][y].offers.length++; // make room for 1 more
+    			tiles[x][y].offerers[tiles[x][y].offerers.length - 1] = msg.sender; // record who is making the offer
+    			tiles[x][y].offers[tiles[x][y].offers.length - 1] = msg.value; // record the offer
     		}	
     	}
     }
     
-    function retractOffer(uint8 col, uint8 row) // retracts the first offer in the array by this user.
+    function retractOffer(uint8 x, uint8 y) // retracts the first offer in the array by this user.
     {
-        for(uint8 i = 0; i < tiles[col][row].offerers.length; i++)
+        for(uint8 i = 0; i < tiles[x][y].offerers.length; i++)
     	{
-    		if(tiles[col][row].offerers[i] == msg.sender) // this user has an offer on file. Remove it.
-    			removeOffer(col,row,i);
+    		if(tiles[x][y].offerers[i] == msg.sender) // this user has an offer on file. Remove it.
+    			removeOffer(x,y,i);
     	}	
     }
     
-    function rejectOffer(uint8 col, uint8 row, uint8 i) // index 0-10
+    function rejectOffer(uint8 x, uint8 y, uint8 i) // index 0-10
     {
-    	if(tiles[col][row].owner != msg.sender) // only the owner can reject offers
+    	if(tiles[x][y].owner != msg.sender) // only the owner can reject offers
     		return;
-    	removeOffer(col,row,i);
+    	removeOffer(x,y,i);
 		return;
     }
     
-    function removeOffer(uint8 col, uint8 row, uint8 i) private // index 0-10, can't be odd
+    function removeOffer(uint8 x, uint8 y, uint8 i) private // index 0-10, can't be odd
     {
     	// return the money
-        tiles[col][row].offerers[i].send(tiles[col][row].offers[i]);
+        tiles[x][y].offerers[i].send(tiles[x][y].offers[i]);
     			
     	// delete user and offer and reshape the array
-    	delete tiles[col][row].offerers[i];   // zero out user
-    	delete tiles[col][row].offers[i];   // zero out offer
-    	for(uint8 j = i+1; j < tiles[col][row].offerers.length; j++) // close the arrays after the gap
+    	delete tiles[x][y].offerers[i];   // zero out user
+    	delete tiles[x][y].offers[i];   // zero out offer
+    	for(uint8 j = i+1; j < tiles[x][y].offerers.length; j++) // close the arrays after the gap
     	{
-    	    tiles[col][row].offerers[j-1] = tiles[col][row].offerers[j];
-    	    tiles[col][row].offers[j-1] = tiles[col][row].offers[j];
+    	    tiles[x][y].offerers[j-1] = tiles[x][y].offerers[j];
+    	    tiles[x][y].offers[j-1] = tiles[x][y].offers[j];
     	}
-    	tiles[col][row].offerers.length--;
-    	tiles[col][row].offers.length--;
+    	tiles[x][y].offerers.length--;
+    	tiles[x][y].offers.length--;
     	return;
     }
     
-    function acceptOffer(uint8 col, uint8 row, uint8 i) // accepts the offer at index (1-10)
+    function acceptOffer(uint8 x, uint8 y, uint8 i) // accepts the offer at index (1-10)
     {
-    	tiles[col][row].owner.send(tiles[col][row].offers[i]); // send offer money to oldowner
-    	tiles[col][row].owner = tiles[col][row].offerers[i]; // new owner is the offerer
-    	delete tiles[col][row].offerers; // delete all offerers
-    	delete tiles[col][row].offers; // delete all offers
+    	tiles[x][y].owner.send(tiles[x][y].offers[i]); // send offer money to oldowner
+    	tiles[x][y].owner = tiles[x][y].offerers[i]; // new owner is the offerer
+    	delete tiles[x][y].offerers; // delete all offerers
+    	delete tiles[x][y].offers; // delete all offers
     	return;
     }
     
-    function getOfferers(uint8 col, uint8 row) constant returns (address[])
+    function getOfferers(uint8 x, uint8 y) constant returns (address[])
     {
-    	return tiles[col][row].offerers;
+    	return tiles[x][y].offerers;
     }
     
-    function getOffers(uint8 col, uint8 row) constant returns (uint[])
+    function getOffers(uint8 x, uint8 y) constant returns (uint[])
     {
-    	return tiles[col][row].offers;
+    	return tiles[x][y].offers;
     }
-    
 }
